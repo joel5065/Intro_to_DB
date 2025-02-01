4fa38700161a320a98c86298c8304b2d980695b5
Joel Kouakou <kouakoujoel7@gmail.com>
2025-02-01 10:11:48 +0000

    adding mysqlserver v002

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,17 +1,29 @@
 import mysql.connector
-
-mydb = mysql.connector.connect (
-    host = "localhost",
-    username = "root",
-    password = "password1234",
-    database = "alx_book_store"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database alx_book_store is created successfully!")
+from mysql.connector import Error
 
 
-mycursor.close()
-mydb.close()
+def create_database():
+    try:
+        mydb = mysql.connector.connect (
+        host = "localhost",
+        username = "root",
+        password = "password1234",
+        database = "alx_book_store"
+        )
+
+        if mydb.is_connected():
+            mycursor = mydb.cursor()
+            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database alx_book_store is created successfully!")
+
+    except Error as e:
+        print(f"Error: {e}")
+    
+    finally:
+        if mydb.is_connected():
+            mycursor.close()
+            mydb.close()
+            print("Connection is closed!")
+
+if __name__ == "__main__":
+    create_database()
